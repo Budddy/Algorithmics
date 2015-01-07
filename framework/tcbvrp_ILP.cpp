184c8421da637b86d9c0fce41b02ae3e5a5ca49f
Markus Zisser <markus.zisser@gmx.at>
2015-01-07 22:39:57 +0100

    added SCF

--- framework/tcbvrp_ILP.cpp
+++ framework/tcbvrp_ILP.cpp
@@ -40,7 +40,7 @@ void tcbvrp_ILP::solve()
 		cplex = IloCplex( model );
 
 		// export model to a text file
-		//cplex.exportModel( "model.lp" );
+		cplex.exportModel( "model.lp" );
 
 		// set parameters
 		setCPLEXParameters();
@@ -54,8 +54,15 @@ void tcbvrp_ILP::solve()
 		cout << "Objective value: " << cplex.getObjValue() << "\n";
 		cout << "CPU time: " << Tools::CPUtime() << "\n\n";
 
-		if( false ) {
-			// TODO optionally output the values of the variables
+		//Copied from: http://orinanobworld.blogspot.co.at/2010/08/iterating-over-cplex-model-sequel.html
+		for (IloIterator<IloNumVar>  it(env); it.ok(); ++it) {
+			try {
+			IloNumVar ext = *it;
+			if(cplex.getValue(ext)!=0)
+				cout << ext.getName() << ": " << cplex.getValue(ext) << "\n";
+			} catch (IloException &e){
+				//cerr << "Exception for variable " << e << "\n";
+			}
 		}
 	}
 	catch( IloException& e ) {
@@ -88,16 +95,323 @@ void tcbvrp_ILP::setCPLEXParameters()
 
 void tcbvrp_ILP::modelSCF()
 {
-	//++++++++++++++++++++++++++++++++++++++++++
-	//TODO build single commodity flow model
-	//++++++++++++++++++++++++++++++++++++++++++
-}
 
-void tcbvrp_ILP::modelMCF()
-{
-	// ++++++++++++++++++++++++++++++++++++++++++
-	// TODO build multi commodity flow model
-	// ++++++++++++++++++++++++++++++++++++++++++
+	// <objective function>
+	int i,j,k;
+	typedef IloArray<IloBoolVarArray> BoolVarMatrix;
+	typedef IloArray<BoolVarMatrix>   BoolVar3Matrix;
+	/* define the num vars here for the 3-D matrix */
+	BoolVar3Matrix objFunc_t(env,instance.m);
+	/* initialize this matrix */
+	for(i=0; i < instance.m; i++)
+	{
+		objFunc_t[i] = BoolVarMatrix(env, instance.n);
+		for(j=0; j < instance.n; j++)
+		{
+			objFunc_t[i][j] = IloBoolVarArray(env, instance.n);
+			for(k=0; k < instance.n; k++)
+			{
+				objFunc_t[i][j][k] = IloBoolVar(env, Tools::indicesToString( "t_", i, j, k).c_str());
+			}
+		}
+	}
+
+
+	typedef IloArray<IloNumVarArray> NumVarMatrix;
+	typedef IloArray<NumVarMatrix>   NumVar3Matrix;
+	NumVar3Matrix objFunc_f(env,instance.m);
+	/* initialize this matrix */
+	for(i=0; i < instance.m; i++)
+	{
+		objFunc_f[i] = NumVarMatrix(env, instance.n);
+		for(j=0; j < instance.n; j++)
+		{
+			objFunc_f[i][j] = IloNumVarArray(env, instance.n);
+			for(k=0; k < instance.n; k++)
+			{
+				objFunc_f[i][j][k] = IloNumVar(env, Tools::indicesToString( "f_", i, j, k).c_str());
+			}
+		}
+	}
+
+	IloExpr objFunction(env);
+	for(i=0;i<instance.m;i++)
+	{
+		for(j=0; j< instance.n; j++)
+		{
+			for(k=0; k< instance.n; k++)
+			{
+				objFunction += objFunc_t[i][j][k] * instance.getDistance(j, k);
+			}
+		}
+	}
+
+	model.add(IloMinimize(env, objFunction));
+	objFunction.end();
+
+	// </objective function>
+
+	// <constraints>
+
+
+//////////
+
+	for(j=0; j< instance.n; j++)
+	{
+		if(instance.isDemandNode(j))
+		{
+			IloExpr myExpr(env);
+			for(i=0;i<instance.m;i++)
+			{
+				for(k=0; k < instance.n; k++)
+				{
+					if(k == 0 || instance.isSupplyNode(k))
+					{
+						myExpr += objFunc_t[i][j][k];
+					}
+				}
+			}
+			model.add(myExpr == 1);
+			myExpr.end();
+		}
+	}
+
+//////////
+
+	for(j=0; j< instance.n; j++)
+	{
+		if(instance.isSupplyNode(j))
+		{
+			IloExpr myExpr1(env);
+			for(i=0;i<instance.m;i++)
+			{
+				for(k=0; k < instance.n; k++)
+				{
+					if(instance.isDemandNode(k))
+					{
+						myExpr1 += objFunc_t[i][j][k];
+					}
+				}
+			}
+			model.add(myExpr1 <= 1);
+			myExpr1.end();
+		}
+	}
+
+
+//////////
+
+	for(j=0; j< instance.n; j++)
+	{
+		if(instance.isSupplyNode(j))
+		{
+			IloExpr myExpr2(env);
+			for(i=0;i<instance.m;i++)
+			{
+				for(k=0; k < instance.n; k++)
+				{
+					if(k == 0 || instance.isSupplyNode(k))
+					{
+						myExpr2 += objFunc_t[i][j][k];
+					}
+				}
+			}
+			model.add(myExpr2 == 0);
+			myExpr2.end();
+		}
+	}
+
+//////////
+
+	IloExpr myExpr3(env);
+	for(i=0;i<instance.m;i++)
+	{
+		for(k=0; k < instance.n; k++)
+		{
+			if(instance.isSupplyNode(k))
+			{
+				myExpr3 += objFunc_t[i][0][k];
+			}
+		}
+	}
+	model.add(myExpr3 <= instance.m);
+	myExpr3.end();
+
+//////////
+
+
+	for(j=0; j< instance.n; j++)
+	{
+		for(i=0;i<instance.m;i++)
+		{
+			IloExpr myExpr4(env);
+			IloExpr myExpr5(env);
+			for(k=0; k < instance.n; k++)
+			{
+				myExpr4 += objFunc_t[i][k][j];
+
+			}
+
+			for(k=0; k < instance.n; k++)
+			{
+				myExpr5 += objFunc_t[i][j][k];
+			}
+
+			model.add(myExpr4 == myExpr5);
+			myExpr4.end();
+			myExpr5.end();
+		}
+	}
+
+//////////
+
+
+	IloExpr myExpr6(env);
+	for(i=0;i<instance.m;i++)
+	{
+		for(k=0; k < instance.n; k++)
+		{
+			if(instance.isDemandNode(k))
+			{
+				myExpr6 += objFunc_t[i][0][k];
+			}
+
+		}
+
+	}
+	model.add(myExpr6 == 0);
+	myExpr6.end();
+
+//////////
+
+	for(i=0;i<instance.m;i++)
+	{
+		IloExpr myExpr7(env);
+		for(j=0;j<instance.n;j++)
+		{
+			for(k=0; k < instance.n; k++)
+			{
+				myExpr7 += objFunc_t[i][j][k] * instance.getDistance(j, k);
+
+			}
+
+		}
+		model.add(myExpr7 <= instance.T);
+		myExpr7.end();
+	}
+
+	//SCF
+
+	for(i=0;i<instance.m;i++)
+	{
+		IloExpr myExpr8(env);
+		IloExpr myExpr9(env);
+		for(j=1;j<instance.n;j++)
+		{
+			myExpr8 += objFunc_f[i][0][j];
+		}
+		for(j=0;j<instance.n;j++)
+		{
+			for(k=0; k < instance.n; k++)
+			{
+				myExpr9 += objFunc_t[i][j][k];
+			}
+
+		}
+		model.add(myExpr8 == myExpr9 - 1);
+		myExpr8.end();
+		myExpr9.end();
+	}
+
+////////////////
+
+	for(i=0;i<instance.m;i++)
+	{
+		for(j=1;j<instance.n;j++)
+		{
+			IloExpr myExpr10(env);
+			IloExpr myExpr11(env);
+			for(k=0; k < instance.n; k++)
+			{
+				if(j!=k)
+				{
+					myExpr10 += objFunc_f[i][k][j];
+				}
+
+			}
+			for(k=0; k < instance.n; k++)
+			{
+				if(j!=k)
+				{
+					myExpr11 += objFunc_f[i][j][k];
+				}
+
+			}
+			model.add(myExpr10 - myExpr11 == 1);
+			myExpr10.end();
+			myExpr11.end();
+
+
+		}
+	}
+
+////////////////
+
+	for(i=0;i<instance.m;i++)
+	{
+		for(j=0;j<instance.n;j++)
+		{
+			for(k=0; k < instance.n; k++)
+			{
+				if(j!=k)
+				{
+					IloExpr myExpr12(env);
+					myExpr12 += objFunc_f[i][j][k];
+
+					model.add(myExpr12 >= 0);
+					myExpr12.end();
+				}
+
+			}
+
+		}
+	}
+
+///////////////////
+
+	for(i=0;i<instance.m;i++)
+	{
+		IloExpr myExpr13(env);
+
+		for(j=0;j<instance.n;j++)
+		{
+			for(k=0; k < instance.n; k++)
+			{
+				myExpr13 += objFunc_t[i][j][k];
+
+			}
+
+		}
+		for(j=0;j<instance.n;j++)
+		{
+			for(k=0; k < instance.n; k++)
+			{
+				if(j!=k)
+				{
+					IloExpr myExpr14(env);
+					myExpr14 += objFunc_f[i][j][k];
+
+					model.add(myExpr14 <= (myExpr13-1)*objFunc_t[i][j][k]);
+					myExpr14.end();
+				}
+
+			}
+
+		}
+		myExpr13.end();
+	}
+
+	// <constraints>
 }
 
 void tcbvrp_ILP::modelMTZ()
@@ -107,6 +421,12 @@ void tcbvrp_ILP::modelMTZ()
 	// ++++++++++++++++++++++++++++++++++++++++++
 }
 
+void tcbvrp_ILP::modelMCF()
+{
+	// ++++++++++++++++++++++++++++++++++++++++++
+	// TODO build multi commodity flow model
+	// ++++++++++++++++++++++++++++++++++++++++++
+}
 
 
 
