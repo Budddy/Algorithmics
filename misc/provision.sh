7791878265cf364a245d6cfcfea01d261b3f8275
Markus Zisser <markus.zisser@gmx.at>
2015-01-07 22:39:43 +0100

    added g++ to provision script

--- misc/provision.sh
+++ misc/provision.sh
@@ -9,4 +9,7 @@ apt-get update > /dev/null
 echo "Installing Java..."
 apt-get install openjdk-7-jre-headless -y
 
+echo "installing g++"
+apt-get install g++ -y
+
 echo "Finished provisioning. Elasticsearch: http://localhost:9200/_plugin/head/; NodeJS: http://localhost:3000"
